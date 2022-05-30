1260eb087926929de3e46cb231aae323212983c8
shaolianfk <871089057@qq.com>
2022-05-30 14:04:08 +0800

    [restruct 🎨] 调整版本检查和自动唤醒的默认值

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -4,6 +4,7 @@ const { program } = require('commander')
 const { createProject, checkNodeVersion, checkNewCliVersion } = require('@slfk/cli-tools')
 
 checkNodeVersion(pkg.engines.node, 'slfk create')
+checkNewCliVersion(pkg.version)
 
 program
   .version(pkg.version, '-v, --version', '查看版本')
@@ -14,10 +15,9 @@ program
   .option('-t, --template <template>', '模板的tag标签')
   .option('-d, --dir <targetDir>', '生成的目标文件夹', false)
   .option('-f, --force', '强制生成', false)
-  .option('-w, --wake', '生成结束后唤醒vscode（需要在全局增加code命令）', true)
+  .option('-w, --wake', '生成结束后唤醒vscode（需要在全局增加code命令）', false)
   .description('创建一个工程')
   .action((name, options) => {
-    checkNewCliVersion(pkg.version)
     createProject(name, options)
   })
 
